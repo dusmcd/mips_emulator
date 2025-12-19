133f55480fbbdd7395df122928756821406cdd72
Dustin <dustinmcdowell1989@gmail.com>
2025-12-19 15:34:31 -0500

    reading instructions from binary file; successful compilation

--- main.go
+++ main.go
@@ -3,16 +3,44 @@ package main
 import (
 	"fmt"
 	"mips_emulator/cpu"
+	"mips_emulator/defs"
+	"os"
+	"log"
 )
+
+func ReadInstructions(filePath string, initialAddr uint32, cpu *cpu.CPU) (int, error) {
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		return 0, err
+	}
+
+	instructions := []uint32{}
+	for i := 0; i < len(data); i += 4{
+		var instr uint32 = 0
+		instr = instr | uint32(data[i]) // least significant byte
+		instr = instr | uint32(data[i + 1]) << 8
+		instr = instr | uint32(data[i + 2]) << 16
+		instr = instr | uint32(data[i + 3]) << 24 // most siginificant byte
+		instructions = append(instructions, instr)
+	}
+
+	for _, instr := range instructions {
+		cpu.MainMemory.LoadInstruction(initialAddr, defs.Word(instr))
+	}
+
+	return len(instructions), nil
+}
 
 func main() {
 
 	fmt.Printf("This is a MIPS Emulator\n")
 	cpu := cpu.InitCPU()
 	initialAddr := uint32(0x01)
-	numInstructions := 2
-	cpu.MainMemory.LoadInstruction(initialAddr, 0x01285020)
-	cpu.MainMemory.LoadInstruction(initialAddr + 4, 0x01285022)
+	numInstructions, err := ReadInstructions("scripts/bin", initialAddr, &cpu)
+	if err != nil {
+		log.Fatalf("fatal error")
+	}
+
 	cpu.Run(numInstructions, initialAddr)
 
 	fmt.Println("Registers")
