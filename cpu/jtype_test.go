815cf7c253352d2b88d78d530ce357bec7c41005
Dustin <dustinmcdowell1989@gmail.com>
2026-01-15 12:26:45 -0500

    wrote and passed tests for jal instruction

--- cpu/jtype_test.go
+++ cpu/jtype_test.go
@@ -5,6 +5,23 @@ import (
 	"mips_emulator/memory"
 )
 
+func TestJal(t *testing.T) {
+	cpu := InitCPU(memory.InitMemory(), 0)
+	cpu.PC = 0x04
+
+	// jal done
+	cpu.Instruction = 0x0C0003e8
+	cpu.DecodeInstr()
+
+	if cpu.PC != 4000 {
+		t.Errorf("PC wrong. expected=%d, got=%d", 4000, cpu.PC)
+	}
+
+	if cpu.Registers[31] != 0x04 {
+		t.Errorf("$ra wrong. expected=%d, got=%d", 0x04, cpu.Registers[31])
+	}
+}
+
 func TestJump(t *testing.T) {
 	cpu := InitCPU(memory.InitMemory(), 0)
 	cpu.PC = 0x04
